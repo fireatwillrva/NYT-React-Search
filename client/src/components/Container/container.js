9d5624559c868a8854cda76db4390d75cccac4c7
William Fisher <fireatwillphoto@live.com>
2018-06-16 11:01:00 -0400

    fixed container component

--- client/src/components/Container/container.js
+++ client/src/components/Container/container.js
@@ -1,11 +1,13 @@
 import React from "react";
 
     const Container = React.createClass({
-        render: function() {
-            return (
-              <div id='article'></div>
-              <div id='note'></div>
-            )
+        render() {
+          return (
+            <div id='article'></div> + 
+            <div id='note'></div>
+          );
         }
-    })
-    ReactDOM.render(<Container />, document.querySelector('#app'))
+      });
+      
+      export default Container;
+      
